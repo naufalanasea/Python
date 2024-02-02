4948645c4653330e6193f7e06aa516c82e21b95c
Naufal Anas <92364025+naufalanasea@users.noreply.github.com>
2024-02-03 00:51:56 +0700

    Update fidrich.py
    menambahkan deskrpisi rumus

--- fidrich.py
+++ fidrich.py
@@ -25,3 +25,8 @@ pll(cube)
 
 # Verifikasi solusi
 assert cube.is_solved()
+
+#Cross: Membentuk salib putih di sisi atas.
+#F2L: Menyelesaikan dua lapisan pertama (F2L) secara berpasangan.
+#OLL: Mengorientasikan semua layer terakhir (OLL).
+#PLL: Memposisikan semua layer terakhir (PLL).
